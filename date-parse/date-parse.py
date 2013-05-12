260f5e8eee9dd856ce9c57b95c331416962e0a84
S Anand <root.node@gmail.com>
2013-05-12 08:39:20 +0530

    Added lookup based approach, which is by far the fastest.

--- date-parse/date-parse.py
+++ date-parse/date-parse.py
@@ -9,9 +9,20 @@ def timing(label, fn):
     t1 = time.time()
     print '%s: %d ms' % (label, int((t1 - t0) * 1000))
 
+def lookup(s):
+    """
+    This is an extremely fast approach to datetime parsing.
+    For large data, the same dates are often repeated. Rather than
+    re-parse these, we store all unique dates, parse them, and
+    use a lookup to convert all dates.
+    """
+    dates = {date:pd.to_datetime(date) for date in s.unique()}
+    return s.apply(lambda v: dates[v])
+
 s = pd.Series(['01-31-2012']*100000)
 
 timing('to_datetime', lambda: pd.to_datetime(s))
 timing('dateutil', lambda: s.apply(dateutil.parser.parse))
 timing('strptime', lambda: s.apply(lambda v: datetime.datetime.strptime(v, '%m-%d-%Y')))
 timing('manual', lambda: s.apply(lambda v: datetime.datetime(int(v[6:10]), int(v[0:2]), int(v[3:5]))))
+timing('lookup', lambda: lookup(s))
